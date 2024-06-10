b18dbf1fa42764b4f8060ccca9e974a69628415b
IbnuParvez <parvezfatma98@gmail.com>
2024-06-10 01:13:49 -0700

    tasks

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
-const fs = require('fs');
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
 
-const fArg = fs.readFileSync(process.argv[2]).toString();
-const sArg = fs.readFileSync(process.argv[3]).toString();
-fs.writeFileSync(process.argv[4], fArg + sArg);
+module.exports = Rectangle;
